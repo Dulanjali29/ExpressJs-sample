fbb4587e012db30ab9249b83781d77473b0c318f
Dulanjali29 <dulanjiariyarathna@gmail.com>
2024-08-09 15:34:50 +0530

    json middleware ok

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express=require('express');
 const app=express();
 
+
 const user=require('./routers/user');
 const student=require('./routers/student');
 
 // app.use(log);
 app.use(express.json());
-app.use('/api/user',log,user);
+// app.use('/api/user',log,user);
+app.use('/api/user',user);
 app.use('/api/student',student);
 
 function log(req,res,next){
@@ -14,7 +16,9 @@ function log(req,res,next){
     req.id="10";
     next();   
 }
-console.log( process.env.PORT);
+// console.log( process.env.PORT);
+ 
+
 const port=process.env.PORT || 3000;
 
 
