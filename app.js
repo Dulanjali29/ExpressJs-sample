75a0c78d3cc9fec89ce134de34a30fa3295b061d
Dulanjali29 <dulanjiariyarathna@gmail.com>
2024-08-09 12:23:27 +0530

    view running port

--- app.js
+++ app.js
@@ -11,6 +11,19 @@ app.post('/api/user',(req,res)=>{
     res.send({'msg':'hello post'});
 });
 
-app.listen(3000);
+app.get('/api/student',(req,res)=>{
+
+    res.send({'msg':'hello Student'});
+
+});
+
+app.post('/api/student',(req,res)=>{
+    res.send({'msg':'hello post from Student '});
+});
+console.log( process.env.PORT);
+const port=process.env.PORT || 3000;
+
+
+app.listen(port,()=>console.log(`App is Running on port ${port} `));
 
 
