7aef1757e516352ab2cd6efc9c938fccdd433172
Dulanjali29 <dulanjiariyarathna@gmail.com>
2024-08-09 14:27:13 +0530

    add middle ware

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const app=express();
 
 const user=require('./routers/user');
 const student=require('./routers/student');
+app.use(log);
 app.use('/api/user',user);
 app.use('/api/student',student);
 
-
+function log(req,res,next){
+    console.log("hello world for middle");
+    next();   
+}
 console.log( process.env.PORT);
 const port=process.env.PORT || 3000;
 
