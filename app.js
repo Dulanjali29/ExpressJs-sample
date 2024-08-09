a1f7093f7eb128d4ab9f74c9947f559fc5407097
Dulanjali29 <dulanjiariyarathna@gmail.com>
2024-08-09 12:11:38 +0530

    get and post api ok

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express=require('express');
 const app=express();
 
-app.get('/api/user/afsd',(req,res)=>{
+app.get('/api/user',(req,res)=>{
 
-res.status(404).send({'msg':'Hello'});
+    res.send({'msg':'hello Get'});
 
+});
+
+app.post('/api/user',(req,res)=>{
+    res.send({'msg':'hello post'});
 });
 
 app.listen(3000);
